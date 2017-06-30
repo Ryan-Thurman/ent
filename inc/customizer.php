7efd5478b2a5c22cdc59bc6970c5454f24f4178b
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-06-30 10:13:00 -0700

    first commit

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * ide_ent Theme Customizer
+ *
+ * @package ide_ent
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function ide_ent_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'ide_ent_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'ide_ent_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'ide_ent_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function ide_ent_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function ide_ent_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function ide_ent_customize_preview_js() {
+	wp_enqueue_script( 'ide_ent_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'ide_ent_customize_preview_js' );
