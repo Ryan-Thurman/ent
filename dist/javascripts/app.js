342d9d3ea71236935bdcd08c48125e9698c75dd8
Ryan Thurman <Ryan-Thurman@users.noreply.github.com>
2017-09-11 12:06:32 -0700

    finishing up registration and login

--- dist/javascripts/app.js
+++ dist/javascripts/app.js
@@ -10597,6 +10597,48 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
   })();
 });
+require.register("javascript/ajax-login-script.js", function(exports, require, module) {
+jQuery(document).ready(function($) {
+
+    // Show the login dialog box on click
+    $('a#show_login').on('click', function(e){
+        $('body').prepend('<div class="login_overlay"></div>');
+        $('form#login').fadeIn(500);
+        $('div.login_overlay, form#login a.close').on('click', function(){
+            $('div.login_overlay').remove();
+            $('form#login').hide();
+        });
+        e.preventDefault();
+    });
+
+    // Perform AJAX login on form submit
+    $('form#login').on('submit', function(e){
+        $('form#login p.status').show().text(ajax_login_object.loadingmessage);
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: ajax_login_object.ajaxurl,
+            data: { 
+                'action': 'ajaxlogin', //calls wp_ajax_nopriv_ajaxlogin
+                'username': $('form#login #username').val(), 
+                'password': $('form#login #password').val(), 
+                'security': $('form#login #security').val() },
+            success: function(data){
+                $('form#login p.status').text(data.message);
+                if (data.loggedin == true){
+                    document.location.href = '/account';
+                } else {
+									console.log('didnt work')
+								}
+            }
+        });
+        e.preventDefault();
+    });
+
+});
+
+});
+
 require.register("javascript/search.js", function(exports, require, module) {
 $(document).ready(function(){
 	
--- functions.php
+++ functions.php
@@ -100,3 +100,44 @@ class StarterSite extends TimberSite {
 
 new StarterSite();
 
+function ajax_login_init(){
+
+    wp_register_script('ajax-login-script', get_template_directory_uri() . '/src/javascript/ajax-login-script.js', array('jquery') ); 
+    wp_enqueue_script('ajax-login-script');
+
+    wp_localize_script( 'ajax-login-script', 'ajax_login_object', array( 
+        'ajaxurl' => home_url() . '/wp-admin/admin-ajax.php',
+        'redirecturl' => home_url(),
+        'loadingmessage' => __('Sending user info, please wait...')
+    ));
+
+    // Enable the user with no privileges to run ajax_login() in AJAX
+    add_action( 'wp_ajax_nopriv_ajaxlogin', 'ajax_login' );
+}
+
+if (!is_user_logged_in()) {
+    add_action('init', 'ajax_login_init');
+}
+
+function ajax_login(){
+
+    // First check the nonce, if it fails the function will break
+    wp_verify_nonce( 'ajax-login-nonce', 'idwe_login_security' );
+
+    // Nonce is checked, get the POST data and sign user on
+    $info = array();
+    $info['user_login'] = $_POST['username'];
+    $info['user_password'] = $_POST['password'];
+    $info['remember'] = true;
+
+    $user_signon = wp_signon( $info, false );
+    if ( is_wp_error($user_signon) ){
+        echo json_encode(array('loggedin'=>false, 'message'=>__('Wrong username or password.')));
+    } else {
+				wp_set_current_user($user_signon->ID);
+        echo json_encode(array('loggedin'=>true, 'message'=>__('Login successful, redirecting...')));
+    }
+
+    die();
+}
+
